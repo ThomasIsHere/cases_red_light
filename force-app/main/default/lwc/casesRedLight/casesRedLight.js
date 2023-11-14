c5ddc5fd79bfdc1201c00723266ee2a35ad15c25
Thomas <lavier.thomas@gmail.com>
2023-11-14 14:23:29 +0100

    Display total number of cases in component using a controller class (helper) metho is @wire in the Js code of the LWC

--- force-app/main/default/lwc/casesRedLight/casesRedLight.js
+++ force-app/main/default/lwc/casesRedLight/casesRedLight.js
@@ -1,5 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import countTotalCases from '@salesforce/apex/CaseRedLightController.countTotalCases';
 
 export default class CasesRedLight extends LightningElement {
-    casesNumber = 1000;
+    totalCases;
+
+    @wire(countTotalCases)
+    wiredTotalCases({ error, data }) {
+        if (data) {
+            this.totalCases = data;
+        } else if (error) {
+            console.error(error);
+        }
+    }
 }
