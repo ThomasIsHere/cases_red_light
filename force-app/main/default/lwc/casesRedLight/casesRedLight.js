94771e4485c5926b949c163f59a5bc7bf12bbb2c
Thomas <lavier.thomas@gmail.com>
2023-11-14 23:09:22 +0100

    Return cases with alert ligh color from SF Case Object to UI

--- force-app/main/default/lwc/casesRedLight/casesRedLight.js
+++ force-app/main/default/lwc/casesRedLight/casesRedLight.js
@@ -1,13 +1,24 @@
 import { LightningElement, wire } from 'lwc';
 import countTotalCases from '@salesforce/apex/CaseRedLightController.countTotalCases';
+import getOpenCasesWithOpenedSinceDays from '@salesforce/apex/CaseRedLightController.getOpenCasesWithOpenedSinceDays';
 
 export default class CasesRedLight extends LightningElement {
     totalCases;
+    mapCasesAlertColors;
 
     @wire(countTotalCases)
     wiredTotalCases({ error, data }) {
         if (data) {
             this.totalCases = data;
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    @wire(getOpenCasesWithOpenedSinceDays)
+    wiredGetOpenCasesWithOpenedSinceDays({ error, data }) {
+        if (data) {
+            this.mapCasesAlertColors = data;
         } else if (error) {
             console.error(error);
         }
