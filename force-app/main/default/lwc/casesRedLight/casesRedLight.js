5eb5d057d168f8da20de5c50a79277e4b7add17c
Thomas <lavier.thomas@gmail.com>
2023-11-27 11:15:49 +0100

    Removing totalCount, add a refresh button to refresh the list of cases in the UI

--- force-app/main/default/lwc/casesRedLight/casesRedLight.js
+++ force-app/main/default/lwc/casesRedLight/casesRedLight.js
@@ -1,34 +1,28 @@
 import { LightningElement, wire, api } from 'lwc';
-
-import countTotalCases from '@salesforce/apex/CaseRedLightController.countTotalCases';
+import { refreshApex } from '@salesforce/apex';
 import getOpenCasesWithOpenedSinceDays from '@salesforce/apex/CaseRedLightController.getOpenCasesWithOpenedSinceDays';
 
 export default class CasesRedLight extends LightningElement {
     @api limitOne;
     @api limitTow;
-
-    totalCases;
     mapCasesAlertColors;
+    wiredReponse;
     error;
 
-    @wire(countTotalCases)
-    wiredTotalCases({ error, data }) {
-        if (data) {
-            this.totalCases = data;
-        } else if (error) {
-            this.error = error;
-        }
-    }
-
     @wire(getOpenCasesWithOpenedSinceDays, {limit1: '$limitOne', limit2: '$limitTow'})
-    wiredGetOpenCasesWithOpenedSinceDays({ error, data }) {
+    wiredGetOpenCasesWithOpenedSinceDays({error, data}) {
+        this.wiredReponse = {error, data};
         if (data) {
             this.mapCasesAlertColors = [];
             for (let key of Object.keys(data)) {
                 this.mapCasesAlertColors.push({ key: key, value: data[key] });
             }
         } else if (error) {
-            console.error(error);
+            this.error(error);
         }
+    }
+
+    clickRefresh(){
+        refreshApex(this.wiredReponse);
     }
 }
