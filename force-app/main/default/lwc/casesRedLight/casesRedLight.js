96a379ee31b3f90ecc0f19da73feb9e4a3547ec4
Thomas <lavier.thomas@gmail.com>
2023-11-27 22:10:13 +0100

    Add age in days in label

--- force-app/main/default/lwc/casesRedLight/casesRedLight.js
+++ force-app/main/default/lwc/casesRedLight/casesRedLight.js
@@ -8,6 +8,7 @@ export default class CasesRedLight extends LightningElement {
     mapCasesAlertColors;
     wiredReponse;
     error;
+    ageDays = '';
 
     @wire(getOpenCasesWithOpenedSinceDays, {limit1: '$limitOne', limit2: '$limitTow'})
     wiredGetOpenCasesWithOpenedSinceDays(response) {
@@ -15,7 +16,14 @@ export default class CasesRedLight extends LightningElement {
         if (response.data) {
             this.mapCasesAlertColors = [];
             for (let key of Object.keys(response.data)) {
-                this.mapCasesAlertColors.push({ key: key, value: response.data[key] });
+                if(key =='GREEN'){
+                    this.ageDays = 'Age <= '+ this.limitOne + ' days';
+                }else if(key =='ORANGE'){
+                    this.ageDays = this.limitOne +' days < Age <= '+ this.limitTow + ' days';
+                }else{
+                    this.ageDays = this.limitTow +' days < Age';
+                }
+                this.mapCasesAlertColors.push({ key: key, value: response.data[key], age: this.ageDays});
             }
         } else if (response.error) {
             this.error = response.error;
